94809e5f58f6ab148c3708a81c3de61c7c865a1f
Damian Szarkowski <97030658+Arszer@users.noreply.github.com>
2022-05-23 14:07:23 +0200

    2.1 v2
    Próba nr 2

--- file.py
+++ file.py
@@ -1,7 +1,8 @@
 def greeter(func):
-    name = func()
-    str.capitalize(name)
-    return f'Aloha {name}'
+    def actual_greeter(*args):
+        name = func(*args)
+        return f'Aloha {name.title()}'
+    return actual_greeter
 
 
 def sums_of_str_elements_are_equal(func):
